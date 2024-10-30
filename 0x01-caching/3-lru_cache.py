a37be92033eedad72ef8e819de4332c9489213be
LindaniMags <lindanimags@gmail.com>
2024-10-30 20:06:11 +0000

    3-lru_cache.py

--- 0x01-caching/3-lru_cache.py
+++ 0x01-caching/3-lru_cache.py
@@ -1,19 +1,19 @@
 #!/usr/bin/env python3
 """
-LIFO Caching
+LRU Caching
 """
 from collections import OrderedDict
 from base_caching import BaseCaching
 
 
-class LIFOCache(BaseCaching):
+class LRUCache(BaseCaching):
     """
-    LIFO Cache that inherits from BaseCaching and is a caching system
+    LRU Cache that inherits from BaseCaching and is a caching system
     """
 
     def __init__(self):
         """
-        Initializes the cache.
+        Initialize.
         """
         super().__init__()
         self.cache_data = OrderedDict()
@@ -26,10 +26,12 @@ class LIFOCache(BaseCaching):
             return
         if key not in self.cache_data:
             if len(self.cache_data) + 1 > BaseCaching.MAX_ITEMS:
-                last_item, _ = self.cache_data.popitem(last=True)
-                print(f"DISCARD: {last_item}")
-        self.cache_data[key] = item
-        self.cache_data.move_to_end(key, last=True)
+                lru_item, _ = self.cache_data.popitem(last=True)
+                print("DISCARD:", lru_item)
+            self.cache_data[key] = item
+            self.cache_data.move_to_end(key, last=False)
+        else:
+            self.cache_data[key] = item
 
     def get(self, key):
         """
