824532b780f5e705c54307b35bc1716ed99a9b9f
LindaniMags <lindanimags@gmail.com>
2024-11-05 19:49:35 +0000

    Permissions update

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -1,19 +1,20 @@
 #!/usr/bin/env python3
-"""A Basic Flask app.
+"""
+Basic Flask app
 """
 from flask import Flask, render_template
-
 
 app = Flask(__name__)
 app.url_map.strict_slashes = False
 
 
 @app.route('/')
-def get_index() -> str:
-    """The home/index page.
+def index() -> str:
     """
-    return render_template('0-index.html')
+    Index page
+    """
+    return render_template('index.html')
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host="0.0.0.0", debug=True)
--- 0x02-i18n/1-app.py
+++ 0x02-i18n/1-app.py
@@ -5,19 +5,24 @@ Basic Babel setup
 from flask import Flask, render_template
 from flask_babel import Babel
 
-app = Flask(__name__)
-babel = Babel(app)
 
-
-class Config():
+class Config:
     """
     Config class
     """
-    LANGUAGEES = ["en", "fr"]
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
 
 
-@app.route('/', strict_slashes=False)
-def index():
+app = Flask(__name__)
+app.config.from_object(Config)
+app.url_map.strict_slashes = False
+babel = Babel(app)
+
+
+@app.route('/')
+def index() -> str:
     """
     Index page
     """
